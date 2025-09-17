c5e6e4e0fc148032ad602384a64a31632e6f0e9f
Salah <mosalahkd028@gmail.com>
2025-09-17 00:01:12 -0600

    Add reusable title component

---
+++ client/src/components/Title.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Title = ({ text1, text2 }) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <div className=" text-lg text-gray-500 md:text-3xl lg:text-5xl ">
+        {text1}
+      </div>
+      <div className=" text-lg font-semibold text-black md:text-3xl lg:text-5xl ">
+        {text2}
+      </div>
+      <hr className="w-6" />
+    </div>
+  );
+};
+
+export default Title;
