485c3681b7bca19146596b0b06349f7b2ca3c9e5
Salah <mosalahkd028@gmail.com>
2025-09-25 01:47:50 -0600

    Add a navigation to product page  functionality on click of product item

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { useNavigate } from "react-router-dom";
 
 const ProductItem = ({ id, name, price, image }) => {
   const { currency } = useContext(ShopContext);
+  const navigate = useNavigate();
 
   return (
-    <div key={id}>
+    <div key={id} onClick={() => navigate(`/product/${id}`)}>
       <img src={image} className="w-full" />
       <p className="py-2">{name}</p>
       <p>{price + currency}</p>
