30ff682e43dc0b8c3b5a31070ca8db414103767e
Salah <mosalahkd028@gmail.com>
2025-09-28 00:53:48 -0600

    Add react tostify component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,14 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import Product from "./pages/Product";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div className="container mx-auto px-8 pt-8">
       <NavBar />
+      <ToastContainer />
 
       <Routes>
         <Route path="/" element={<Home />} />
