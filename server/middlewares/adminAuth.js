4d74fb31f33af0b0bc2118b3a48456b908a97123
Salah <mosalahkd028@gmail.com>
2025-10-05 05:56:40 -0600

    Add admin authenticaton middleware

---
+++ server/middlewares/adminAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const adminAuth = async (req, res, next) => {
+  const { token } = req.headers;
+
+  try {
+    if (!token) {
+      return res.json({ success: false, msg: "Not Authorized login again" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // check if the role is not admin
+    if (decoded.role !== "admin") {
+      return res.json({ success: false, msg: "Not Authorized login again" });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, msg: e.message });
+  }
+};
+
+export default adminAuth;
