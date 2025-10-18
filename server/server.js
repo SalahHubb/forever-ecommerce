afad515c7d915d16b6e7f38f7ff4f99794b24018
Salah <mosalahkd028@gmail.com>
2025-10-18 08:03:46 -0600

    fix: update cors config and vercel routing

--- server/server.js
+++ server/server.js
@@ -13,13 +13,8 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
 
 app.get("/", (req, res) => {
   res.send("Forever e-commerce api is working...");
