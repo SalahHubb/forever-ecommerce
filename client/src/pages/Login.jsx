c309d88b8643cac55d1783e29ef192d2a35b17dc
Salah <mosalahkd028@gmail.com>
2025-10-18 00:56:43 -0600

    Add handling login and user and user registration

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,68 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const { backend_url, setToken } = useContext(ShopContext);
+  const navigate = useNavigate();
+
   const [status, setStatus] = useState("login");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // login user
+      if (status == "login") {
+        const response = await axios.post(backend_url + "/api/user/login", {
+          email,
+          password,
+        });
+
+        if (response.data.success) {
+          navigate("/");
+          setToken(response.data.token);
+          setEmail("");
+          setPassword("");
+        } else {
+          toast.error(response.data.msg);
+        }
+      } else {
+        // register user
+        const response = await axios.post(backend_url + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (response.data.success) {
+          navigate("/");
+          setToken(response.data.token);
+          setEmail("");
+          setName("");
+          setPassword("");
+          toast.success(response.data.msg);
+        } else {
+          toast.error(response.data.msg);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center py-18">
-      <div className="w-full flex flex-col gap-4 max-w-[400px]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full flex flex-col gap-4 max-w-[400px]"
+      >
         <div className="flex justify-center">
           {status == "sign up" ? (
             <Title text1={"Sign"} text2={"Up"} />
@@ -18,7 +74,9 @@ const Login = () => {
           type="text"
           name="name"
           placeholder="Name"
-          className={`border-1 border-gray-500 rounded-sm px-4 py-2 ${
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className={`border-1 border-gray-500 rounded-md px-4 py-2 ${
             status == "login" ? "hidden" : "block"
           }`}
         />
@@ -26,27 +84,38 @@ const Login = () => {
           type="email"
           name="email"
           placeholder="Email"
-          className="border-1 border-gray-500 rounded-sm px-4 py-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border-1 border-gray-500 rounded-md px-4 py-2"
         />
         <input
           type="password"
           name="password"
           placeholder="Password"
-          className="border-1 border-gray-500 rounded-sm px-4 py-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border-1 border-gray-500 rounded-md px-4 py-2"
         />
         <div className="flex justify-between">
-          <p>Forgot your password?</p>
+          <p className="hover:cursor-pointer">Forgot your password?</p>
           <p onClick={() => setStatus(status == "login" ? "sign up" : "login")}>
-            {status == "login" ? <p>Create an account</p> : <p>Login here</p>}
+            {status == "login" ? (
+              <p className="hover:cursor-pointer">Create an account</p>
+            ) : (
+              <p className="hover:cursor-pointer">Login here</p>
+            )}
           </p>
         </div>
 
         <div className="flex justify-center mt-4">
-          <button className="bg-black text-white px-4 py-2 rounded-md text-center">
+          <button
+            type="submit"
+            className="bg-black text-white px-4 py-2 rounded-md text-center"
+          >
             {status == "login" ? "Sign in" : "Sign up"}
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
