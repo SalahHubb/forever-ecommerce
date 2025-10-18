92e98b604c49c433b91d9269a9ec850931f2a07d
Salah <mosalahkd028@gmail.com>
2025-10-18 01:18:27 -0600

    Adjust size

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Title = ({ text1, text2 }) => {
   return (
     <div className="flex items-center space-x-2">
-      <div className=" text-lg text-gray-500 md:text-3xl lg:text-5xl ">
+      <div className=" text-lg text-gray-500 md:text-3xl lg:text-4xl ">
         {text1}
       </div>
-      <div className=" text-lg font-semibold text-black md:text-3xl lg:text-5xl ">
+      <div className=" text-lg font-semibold text-black md:text-3xl lg:text-4xl ">
         {text2}
       </div>
       <hr className="w-6" />
