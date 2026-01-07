9f37c42af96a347f06bb86b52cd3f1208cece8f4
Salah <mosalahkd028@gmail.com>
2026-01-07 08:04:17 -0700

    Add comment for clarity

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "node:path";
 
+// the role of multer: to handle multipart/form-data, which is primarily used for uploading files. > Temporarily store the files on the server's disk storage(folder) or memory storage(built in option to store temporarily as a buffer). > Provide access to the uploaded files via req.file or req.files in the route handlers.  
 const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
